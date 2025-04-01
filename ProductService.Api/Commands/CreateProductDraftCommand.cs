82222c12aa49ddc37ec9a650f37d68acf67a1d17
Ccompiler <balabanov-03@list.ru>
2025-04-01 17:57:22 +0300

    Add commands and DTOs for ProductService

---
+++ ProductService.Api/Commands/CreateProductDraftCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ProductService.Api.Commands.Results;
+
+namespace ProductService.Api.Commands;
+
+public record CreateProductDraftCommand() : IRequest<CreateProductDraftResult>;
---
+++ ProductService.Api/Commands/DTOs/ProductDraftDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductService.Api.Commands.DTOs;
+
+public record ProductDraftDto(
+    string Code,
+    string Name,
+    string Image,
+    string Description,
+    int MaxNumberOfInsured,
+    string Icon);
