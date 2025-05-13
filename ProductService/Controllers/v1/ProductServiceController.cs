8c1bfd9c1ccbbb5131b66283a7832931f32e4a0a
ABalabanov <a.balabanov@dunice.net>
2025-05-13 13:04:22 +0300

    [Product-Api] Написал замоканное апи, дальше нужно будет добавлять Request-ы и DTOs

--- ProductService/Controllers/v1/ProductServiceController.cs
+++ ProductService/Controllers/v1/ProductServiceController.cs
@@ -10,11 +10,52 @@ namespace ProductService.Controllers.v1;
 public class ProductServiceController : ControllerBase
 {
     /// <summary>
-    /// Получение всех сущностей
+    /// Получение всех продуктов
     /// </summary>
     /// <returns>Не воз</returns>
-    [HttpGet]
+    [HttpGet("/all")]
     public async Task GetAll()
+    {
+        // Todo
+    }
+
+    /// <summary>
+    /// Получение продукта по коду
+    /// </summary>
+    /// <param name="code">Код продукта</param>
+    /// <returns>Не воз</returns>
+    [HttpGet("{code}")]
+    public async Task GetByCode(string code)
+    {
+        // Todo
+    }
+
+    /// <summary>
+    /// Активация продукта
+    /// </summary>
+    /// <returns>Не воз</returns>
+    [HttpPatch("/activate")]
+    public async Task Activate()
+    {
+        // Todo
+    }
+
+    /// <summary>
+    /// Создание draft-продукта
+    /// </summary>
+    /// <returns>Не воз</returns>
+    [HttpPost]
+    public async Task CreateDraft()
+    {
+        // Todo
+    }
+
+    /// <summary>
+    /// Получение продукта по коду
+    /// </summary>
+    /// <returns>Не воз</returns>
+    [HttpPatch("/discontinue")]
+    public async Task Discontinue()
     {
         // Todo
     }
