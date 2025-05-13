e99214eca88605b3baab3cfcddf7f9e6209d4053
ABalabanov <a.balabanov@dunice.net>
2025-05-13 12:10:27 +0300

    [Product-Api] Добавил конфиг для централизованного управления версиями пакетов, и немного поправил по форматированиям

---
+++ ProductService/Controllers/v1/ProductServiceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers.v1;
+
+/// <summary>
+/// Контроллер сервиса продуктов
+/// </summary>
+[Route("api/v1/product")]
+[ApiController]
+public class ProductServiceController : ControllerBase
+{
+    /// <summary>
+    /// Получение всех сущностей
+    /// </summary>
+    /// <returns>Не воз</returns>
+    [HttpGet]
+    public async Task GetAll()
+    {
+        // Todo
+    }
+}
