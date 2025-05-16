d45d8460df01cd42a0e9653a81f31e285ba13072
ABalabanov <a.balabanov@dunice.net>
2025-05-16 19:46:39 +0300

    [Product-Api] Сделал сущность, добавил референц в ProductService на Persistence сборку с контекстом и сущностями Поправил в .editorconfig: убрал лишний warning

--- ProductService.Persistence/Entities/Choice.cs
+++ ProductService.Persistence/Entities/Choice.cs
@@ -1,6 +1,35 @@
 ﻿namespace ProductService.Persistence.Entities;
 
+/// <summary>
+/// Сущность "Выбора"
+/// </summary>
 public class Choice
 {
-    
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public Choice()
+    {
+    }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="code">Код</param>
+    /// <param name="label">Лэйбл</param>
+    public Choice(string code, string label)
+    {
+        Code = code;
+        Label = label;
+    }
+
+    /// <summary>
+    /// Код
+    /// </summary>
+    public string Code { get; }
+
+    /// <summary>
+    /// Лэйбл
+    /// </summary>
+    public string Label { get; }
 }
