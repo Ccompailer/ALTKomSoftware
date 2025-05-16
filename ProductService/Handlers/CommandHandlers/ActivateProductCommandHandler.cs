1accb5804b337a0232a997f2b9c4faddbb6bde91
ABalabanov <a.balabanov@dunice.net>
2025-05-16 22:16:13 +0300

    [Product-Api] Добавил конструктор с параметрами и описание.

--- ProductService/Handlers/CommandHandlers/ActivateProductCommandHandler.cs
+++ ProductService/Handlers/CommandHandlers/ActivateProductCommandHandler.cs
@@ -1,23 +1,37 @@
 ﻿using MediatR;
 using ProductService.Api.Commands;
 using ProductService.Api.Commands.Results;
+using ProductService.Persistence.AppDbContext;
 
 namespace ProductService.Handlers.CommandHandlers;
 
 /// <summary>
-/// 
+/// Обработчик команды по созданию продукта
 /// </summary>
 public class ActivateProductCommandHandler : IRequestHandler<ActivateProductCommand, ActivateProductResult>
 {
+    private readonly ILogger<ActivateProductCommandHandler> _logger;
+    private readonly IAppContext _appContext;
+
     /// <summary>
-    /// 
+    /// Конструктор класс обработчика
     /// </summary>
-    /// <param name="request"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Task<ActivateProductResult> Handle(ActivateProductCommand request, CancellationToken cancellationToken)
+    /// <param name="appContext">Контекст ProductService</param>
+    /// <param name="logger">Логгер для обработчика</param>
+    public ActivateProductCommandHandler(IAppContext appContext, ILogger<ActivateProductCommandHandler> logger)
     {
-        throw new NotImplementedException();
+        _appContext = appContext;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Основной метод обработчика
+    /// </summary>
+    /// <param name="command">Команда на создание с параметрами</param>
+    /// <param name="cancellationToken">Токен отмены</param>
+    /// <returns>Созданный продукт</returns>
+    public Task<ActivateProductResult> Handle(ActivateProductCommand command, CancellationToken cancellationToken)
+    {
+        
     }
 }
