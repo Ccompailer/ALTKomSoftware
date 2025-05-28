fd095dceb4107e97e146bf28e59d0e600b1652fc
ABalabanov <a.balabanov@dunice.net>
2025-05-29 00:19:34 +0300

    [Product-Api] Сделал сервис для работы с доменными моделями и сокрытием бизнес-логики

---
+++ ProductService/Handlers/Services/IProductFlowService.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductService.Handlers.Services;
+
+/// <summary>
+/// Интерфейс сервиса бизнес-логики ProductService
+/// </summary>
+public interface IProductFlowService
+{
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="productId">Идентификатор продукта</param>
+    /// <param name="ct">Токен отмены <see cref="CancellationToken"/></param>
+    /// <returns><see cref="Task"/></returns>
+    Task ActiveProductAsync(Guid productId, CancellationToken ct);
+}
---
+++ ProductService/Handlers/Services/ProductFlowService.cs
@@ -0,0 +1,30 @@
+﻿using ProductService.Persistence.AppDbContext;
+
+namespace ProductService.Handlers.Services;
+
+/// <summary>
+/// Сервис бизнес-логики ProductService
+/// </summary>
+/// <param name="appContext">Контекст приложения для работы с БД</param>
+/// <param name="logger">Логгер</param>
+public class ProductFlowService(IAppContext appContext, ILogger<ProductFlowService> logger)
+    : IProductFlowService
+{
+    private readonly ILogger<ProductFlowService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    private readonly IAppContext _appContext = appContext ?? throw new ArgumentNullException(nameof(appContext));
+
+    /// <inheritdoc />
+    public async Task ActiveProductAsync(Guid productId, CancellationToken ct)
+    {
+        var product = await _appContext.Products.FindAsync([productId], ct);
+
+        if (product is null)
+        {
+            logger.LogInformation($"Product [{productId}] doesn't exist");
+        }
+
+        // TODO: Add logic for success result.
+        // Call product method for change product status to 'Active'
+        await _appContext.SaveChangesAsync(ct);
+    }
+}
